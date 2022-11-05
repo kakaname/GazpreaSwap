a8bb667c565d76819d567e4a27f8b56cb11847cf
xiaofeiyu2019 <yufei24@ualberta.ca>
2022-11-05 10:10:07 -0600

    added classof() methods

--- include/AST/ASTNodes.h
+++ include/AST/ASTNodes.h
@@ -49,147 +49,205 @@ private:
 
 
 class Program: ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Program;
+    }
 };
 
 
 class Identifier: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Identifier;
+    }
 };
 
 
 class Assignment: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Assignment;
+    }
 };
 
 
 class Declaration: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Declaration;
+    }
 };
 
 
 class Block: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Block;
+    }
 };
 
 
 class LogicalOp: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_LogicalOp;
+    }
 };
 
 
 class ArithmeticOp: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_ArithmeticOp;
+    }
 };
 
 
 class Index: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Index;
+    }
 };
 
 
 class Loop: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Loop;
+    }
 };
 
 
 class IntLiteral: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_IntLiteral;
+    }
 };
 
 
 class NullLiteral: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_NullLiteral;
+    }
 };
 
 
 class IdentityLiteral: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_IdentityLiteral;
+    }
 };
 
 
 class RealLiteral: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_RealLiteral;
+    }
 };
 
 
 class BoolLiteral: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_BoolLiteral;
+    }
 };
 
 
 class CharLiteral: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_CharLiteral;
+    }
 };
 
 
 class Tuple: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Tuple;
+    }
 };
 
 
 class TupleAccess: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_TupleAccess;
+    }
 };
 
 
 class TupleDecl: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_TupleDecl;
+    }
 };
 
 
 class Conditional: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Conditional;
+    }
 };
 
 
 class Cast: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Cast;
+    }
 };
 
 
 class BitwiseOp: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_BitwiseOp;
+    }
 };
 
 
 class UnaryOp: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_UnaryOp;
+    }
 };
 
 
 class FunctionDecl: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_FunctionDecl;
+    }
 };
 
 
 class FunctionCall: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_FunctionCall;
+    }
 };
 
 
 class ProcedureDecl: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_ProcedureDecl;
+    }
 };
 
 
 class ProcedureCall: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_ProcedureCall;
+    }
 };
 
 
 class Return: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Return;
+    }
 };
 
 
 class Break: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Break;
+    }
 };
 
 
 class Continue: public ASTNode {
-
+    static bool classof(const ASTNode *N) {
+        return N->getKind() == ASTNodeKind::N_Continue;
+    }
 };
 
 #endif //GAZPREABASE_ASTNODES_H
