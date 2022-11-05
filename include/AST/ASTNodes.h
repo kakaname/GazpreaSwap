3652e0d0502259b838ee25a6d51c635302a34c35
xiaofeiyu2019 <yufei24@ualberta.ca>
2022-11-04 23:59:42 -0600

    add ASTNode basis

--- include/AST/ASTNodes.h
+++ include/AST/ASTNodes.h
@@ -5,7 +5,164 @@
 #ifndef GAZPREABASE_ASTNODES_H
 #define GAZPREABASE_ASTNODES_H
 
-using ASTNodeT = TreeNode<UnitT>;
 
+class ASTNode {
+public:
+    enum ASTNodeKind {
+        N_Identifier,
+        N_Declaration
+    };
+
+    ASTNodeKind getKind() const {
+        return Kind;
+    }
+private:
+    ASTNodeKind Kind;
+};
+
+
+class Program: ASTNode {
+
+};
+
+
+class Identifier: public ASTNode {
+
+};
+
+
+class Assignment: public ASTNode {
+
+};
+
+
+class Declaration: public ASTNode {
+
+};
+
+
+class Block: public ASTNode {
+
+};
+
+
+class LogicalOp: public ASTNode {
+
+};
+
+
+class ArithmeticOp: public ASTNode {
+
+};
+
+
+class Index: public ASTNode {
+
+};
+
+
+class Loop: public ASTNode {
+
+};
+
+
+class IntLiteral: public ASTNode {
+
+};
+
+
+class NullLiteral: public ASTNode {
+
+};
+
+
+class IdentityLiteral: public ASTNode {
+
+};
+
+
+class RealLiteral: public ASTNode {
+
+};
+
+
+class BoolLiteral: public ASTNode {
+
+};
+
+
+class CharLiteral: public ASTNode {
+
+};
+
+
+class Tuple: public ASTNode {
+
+};
+
+
+class TupleAccess: public ASTNode {
+
+};
+
+
+class TupleDecl: public ASTNode {
+
+};
+
+
+class Conditional: public ASTNode {
+
+};
+
+
+class Cast: public ASTNode {
+
+};
+
+
+class BitwiseOp: public ASTNode {
+
+};
+
+
+class UnaryOp: public ASTNode {
+
+};
+
+
+class FunctionDecl: public ASTNode {
+
+};
+
+
+class FunctionCall: public ASTNode {
+
+};
+
+
+class ProcedureDecl: public ASTNode {
+
+};
+
+
+class ProcedureCall: public ASTNode {
+
+};
+
+
+class Return: public ASTNode {
+
+};
+
+
+class Break: public ASTNode {
+
+};
+
+
+class Continue: public ASTNode {
+    
+};
 
 #endif //GAZPREABASE_ASTNODES_H
